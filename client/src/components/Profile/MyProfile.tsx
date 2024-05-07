988a9c4b84e98fd03114e091c623124379ae51c9
nikolay <koliasinitsa317@gmail.com>
2024-05-07 21:25:42 +0300

    fix button my profile

--- client/src/components/Profile/MyProfile.tsx
+++ client/src/components/Profile/MyProfile.tsx
@@ -3,8 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../Header/Header';
 import ProfileData from './ProfileData';
 import { getDecodedToken } from '../../services/TokenServices';
-import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 const MyProfile: React.FC = () => {
 
@@ -18,12 +18,14 @@ const MyProfile: React.FC = () => {
       <br />
       <div className="container mt-5">
         <h2 className="text-center mb-4" style={{ fontSize: '24px' }}>My Profile</h2>
+        <div style={{display: 'grid', }}>
           <ProfileData UserId={userId} />
-          <Button variant="contained" href="#contained-buttons">
+          <Button variant="contained" href="#contained-buttons" style={{width: "170px", marginTop: "30px", marginLeft: "800px"}} >
             <Link to="/EditProfile" style={{ textDecoration: 'none', color: 'inherit' }}>
               Edit Profile
             </Link>
           </Button>
+        </div>
       </div>
     </div>
   );
