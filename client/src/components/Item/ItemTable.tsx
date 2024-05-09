4387269c473c78a6ff3af80093bf89af51e53191
nikolay <koliasinitsa317@gmail.com>
2024-05-09 14:27:31 +0300

    pagination page

--- client/src/components/Item/ItemTable.tsx
+++ client/src/components/Item/ItemTable.tsx
@@ -1,17 +1,21 @@
-// ItemTable.tsx
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ItemCard from './ItemCard';
 import { Item } from '../../interfaces/ItemCardProps';
 import Spinner from '../Spinner/Spinner';
-
+import Pagination from './Pagination';
 
 const ItemTable: React.FC = () => {
     const [items, setItems] = useState<Item[]>([]);
     const [loading, setLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
-        axios.get('http://localhost:3000/ItemRoutes/getAllCars')
+        fetchData(currentPage);
+    }, [currentPage]);
+
+    const fetchData = (page: number) => {
+        axios.get(`http://localhost:3000/ItemRoutes/getAllCars?pageNumber=${page}&pageSize=6`)
             .then(response => {
                 setItems(response.data);
                 setLoading(false);
@@ -19,19 +23,35 @@ const ItemTable: React.FC = () => {
             .catch(error => {
                 console.error('Error fetching data:', error);
             });
-    }, []);
+    };
+
+    const goToPreviousPage = () => {
+        setCurrentPage(prevPage => Math.max(prevPage - 1, 1));
+    };
+
+    const goToNextPage = () => {
+        setCurrentPage(prevPage => prevPage + 1);
+    };
 
     if (loading) {
         return <Spinner />
     }
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '10px', maxWidth: '960px', margin: '0 auto', padding: '10px -5px' }}>
-                {items.map((item) => (
-                    <ItemCard key={item.id} item={item} />
-                ))}
+        <div>
+            <Pagination 
+                currentPage={currentPage}
+                goToPreviousPage={goToPreviousPage}
+                goToNextPage={goToNextPage}
+            />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '10px', maxWidth: '960px', margin: '0 auto', padding: '10px -5px' }}>
+                    {items.map((item) => (
+                        <ItemCard key={item.id} item={item} />
+                    ))}
+                </div>
             </div>
+            
         </div>
     );
 }
---
+++ client/src/components/Item/Pagination.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+interface PaginationProps {
+    currentPage: number;
+    goToPreviousPage: () => void;
+    goToNextPage: () => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({ currentPage, goToPreviousPage, goToNextPage }) => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+            <button
+                onClick={goToPreviousPage}
+                disabled={currentPage === 1}
+                style={{
+                    fontWeight: 'bold',
+                    fontSize: '18px',
+                    borderRadius: '50%', // Радиус равен половине высоты кнопки
+                    border: '1px solid #dddddd',
+                    width: '40px', // Ширина кнопки
+                    height: '40px', // Высота кнопки
+                    backgroundColor: currentPage === 1 ? '#f5f5f5' : '#fff',
+                    color: currentPage === 1 ? '#ccc' : '#3f51b5',
+                    cursor: currentPage === 1 ? 'not-allowed' : 'pointer',
+                    transition: 'background-color 0.3s, color 0.3s, border-radius 0.3s',
+                    pointerEvents: currentPage === 1 ? 'none' : 'auto',
+                }}
+                onMouseEnter={(e) => {
+                    if (currentPage !== 1) {
+                        e.currentTarget.style.backgroundColor = '#3f51b5';
+                        e.currentTarget.style.color = '#fff';
+                    }
+                }}
+                onMouseLeave={(e) => {
+                    if (currentPage !== 1) {
+                        e.currentTarget.style.backgroundColor = currentPage === 1 ? '#f5f5f5' : '#fff';
+                        e.currentTarget.style.color = currentPage === 1 ? '#ccc' : '#3f51b5';
+                    }
+                }}
+            >
+                {'<'}
+            </button>
+            <span style={{ margin: '0 10px', fontSize: '24px', color: '#3f51b5' }}>{currentPage}</span>
+            <button
+                onClick={goToNextPage}
+                style={{
+                    borderRadius: '50%', // Радиус равен половине высоты кнопки
+                    border: '1px solid #dddddd',
+                    width: '40px', // Ширина кнопки
+                    height: '40px', // Высота кнопки
+                    backgroundColor: '#fff',
+                    color: '#3f51b5',
+                    cursor: 'pointer',
+                    transition: 'background-color 0.3s, color 0.3s, border-radius 0.3s',
+                    fontWeight: 'bold',
+                    fontSize: '18px',
+                }}
+                onMouseEnter={(e) => {
+                    e.currentTarget.style.backgroundColor = '#3f51b5';
+                    e.currentTarget.style.color = '#fff';
+                }}
+                onMouseLeave={(e) => {
+                    e.currentTarget.style.backgroundColor = '#fff';
+                    e.currentTarget.style.color = '#3f51b5';
+                }}
+            >
+                {'>'}
+            </button>
+        </div>
+    );
+}
+
+export default Pagination;
