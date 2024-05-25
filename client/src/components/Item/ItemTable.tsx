f7a45d14117610d5656b84dceb932d611473be60
nikolay <koliasinitsa317@gmail.com>
2024-05-25 23:28:19 +0300

    fix itemtable pagination

--- client/src/components/Item/ItemTable.tsx
+++ client/src/components/Item/ItemTable.tsx
@@ -5,7 +5,6 @@ import { Item } from '../../interfaces/ItemCardProps';
 import Spinner from '../Spinner/Spinner';
 import Pagination from './Pagination';
 
-
 interface ItemTableProps {
     filters: {
         brand: string;
@@ -18,29 +17,45 @@ const ItemTable: React.FC<ItemTableProps> = ({ filters }) => {
     const [items, setItems] = useState<Item[]>([]);
     const [loading, setLoading] = useState(true);
     const [currentPage, setCurrentPage] = useState(1);
+    const [isFiltered, setIsFiltered] = useState(false);
 
     useEffect(() => {
-        fetchData(currentPage);
-    }, [currentPage]);
-
- 
+        if (isFiltered) {
+            fetchFilteredCars(filters);
+        } else {
+            fetchData(currentPage);
+        }
+    }, [currentPage, isFiltered]);
 
     useEffect(() => {
-        fetchFilteredCars(filters);
+        if (filters.brand || filters.bodyType || filters.transmission) {
+            setIsFiltered(true);
+            fetchFilteredCars(filters);
+        } else {
+            setIsFiltered(false);
+            fetchData(currentPage);
+        }
     }, [filters]);
 
     const fetchData = (page: number) => {
-        axios.get(`http://localhost:3000/ItemRoutes/getAllCars?pageNumber=${page}&pageSize=6`)
-            .then(response => {
-                setItems(response.data);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
+        setLoading(true);
+        axios.get(`http://localhost:3000/ItemRoutes/getAllCars`, {
+            params: {
+                pageNumber: page,
+                pageSize: 6,
+            }
+        })
+        .then(response => {
+            setItems(response.data);
+            setLoading(false);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            setLoading(false);
+        });
     };
 
-     const fetchFilteredCars = (filters: { brand: string; bodyType: string; transmission: string }) => {
+    const fetchFilteredCars = (filters: { brand: string; bodyType: string; transmission: string }) => {
         setLoading(true);
         axios.get(`http://localhost:3000/ItemRoutes/cars`, {
             params: {
@@ -49,16 +64,15 @@ const ItemTable: React.FC<ItemTableProps> = ({ filters }) => {
                 transmission: filters.transmission,
             }
         })
-            .then(response => {
-                setItems(response.data);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error('Error fetching filtered data:', error);
-                setLoading(false);
-            });
+        .then(response => {
+            setItems(response.data);
+            setLoading(false);
+        })
+        .catch(error => {
+            console.error('Error fetching filtered data:', error);
+            setLoading(false);
+        });
     };
-
 
     const goToPreviousPage = () => {
         setCurrentPage(prevPage => Math.max(prevPage - 1, 1));
@@ -86,7 +100,6 @@ const ItemTable: React.FC<ItemTableProps> = ({ filters }) => {
                     ))}
                 </div>
             </div>
-
         </div>
     );
 }
